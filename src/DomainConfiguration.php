48e049b47265c417d9856c9cfbf6c043357a8581
Sven Neumann <s.neumann@websix.de>
2018-12-16 19:05:32 +0100

    add option for ports

--- src/DomainConfiguration.php
+++ src/DomainConfiguration.php
@@ -14,24 +14,26 @@ class DomainConfiguration
     public $domains;
     public $servers = array();
     public $username;
+    public $defaultPort;
+    public $defaultSslPort;
 
-    public function addServer($type, $hostname)
+    public function addServer($type, $hostname, $defaulPort = null, $defaultSslPort = null)
     {
-        $server = $this->createServer($type, $hostname);
+        $server = $this->createServer($type, $hostname, $defaulPort, $defaultSslPort);
         $server->username = $this->username;
         array_push($this->servers, $server);
         return $server;
     }
 
-    private function createServer($type, $hostname)
+    private function createServer($type, $hostname, $defaultPort, $defaultSslPort)
     {
         switch ($type) {
             case 'imap':
-                return new Server($type, $hostname, 143, 993);
+                return new Server($type, $hostname, !is_null($defaultPort) ? $defaultPort : 143, !is_null($defaultSslPort) ? $defaultSslPort : 993);
             case 'pop3':
-                return new Server($type, $hostname, 110, 995);
+                return new Server($type, $hostname, !is_null($defaultPort) ? $defaultPort : 110, !is_null($defaultSslPort) ? $defaultSslPort : 995);
             case 'smtp':
-                return new Server($type, $hostname, 25, 465);
+                return new Server($type, $hostname, !is_null($defaultPort) ? $defaultPort : 25, !is_null($defaultSslPort) ? $defaultSslPort : 465);
             default:
                 throw new Exception("Unrecognized server type \"$type\"");
         }
